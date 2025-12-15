f324f6c78c7875954275938cd844f864cb04c3a1
misterveyda <veydaszn@gmail.com>
2025-12-15 19:52:33 +0000

    commit

--- docs/script.js
+++ docs/script.js
@@ -99,19 +99,22 @@ function populateClassTable(learners) {
 
     learners.forEach(l => {
         const subjects = l.subjects;
-        let totalScore = 0;
-        Object.keys(subjects).forEach(sub => {
-            const grade = subjects[sub];
-            const score = gradeToNumber(grade);
-            totalScore += score;
-            const tr = document.createElement("tr");
-            tr.innerHTML = `<td>${l.learner}</td><td>${sub}</td><td>${grade}</td><td>${score}</td>`;
-            tbody.appendChild(tr);
+        const subjectScores = Object.values(subjects).map(gradeToNumber);
+        
+        // Calculate the total score
+        const totalScore = subjectScores.reduce((sum, score) => sum + score, 0);
+        
+        // Calculate the percentage based on Max 72
+        const percentage = ((totalScore / MAX_TOTAL_SCORE) * 100).toFixed(1); 
+
+        const tr = document.createElement("tr");
+        let tdContent = `<td>${l.learner}</td>`;
+        Object.values(subjects).forEach(grade => {
+            tdContent += `<td>${grade}</td>`;
         });
-        // Add total row
-        const trTotal = document.createElement("tr");
-        trTotal.innerHTML = `<td colspan="3"><strong>Total</strong></td><td><strong>${totalScore}</strong></td>`;
-        tbody.appendChild(trTotal);
+        tdContent += `<td>${totalScore} / ${MAX_TOTAL_SCORE}</td><td>${percentage}%</td>`;
+        tr.innerHTML = tdContent;
+        tbody.appendChild(tr);
     });
 }
 
